22f3bd500907faff5eb74a6e1fb7811c66dd8b41
julioscasme <jdk.jdcm99@gmail.com>
2024-02-08 14:27:41 -0500

    primera subida de calculadora

---
+++ src/componentes/BontonClear.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../style/BotonClear.css'
+
+const BotonClear = (props) => (
+
+    <div className="boton-clear">
+        {props.children}
+    </div>
+
+);
+export default BotonClear;
